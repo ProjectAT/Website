878d80c677fbb42d8433211f56acef4a49ffaa74
Brandon <brandonmowat11@gmail.com>
2016-06-15 15:45:11 -0400

    Updated button component, card component, index, and styles.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,35 @@
-// This is script.js
-// You can write JavaScript for your site in here.
-//
+var navcontent = '<a href="#">About</a><a href="#">Team</a><a href="https://github.com/ProjectAT/uam" target="">Github</a>'
+
+function loadContent(destination, content) {
+	console.log(destination);
+	var target = document.getElementById(destination);
+	target.innerHTML = '<object type="text/html" data="'+ content +'" ></object>';
+	changeNavToClose();
+}
+
+function changeNavToClose() {
+	var nav = document.getElementById('nav');
+	console.log(nav.innerHTML);
+	nav.innerHTML = '<a onclick="closeSampleOutput()">close</a>';
+}
+
+function openSampleOutput() {
+	var el = document.getElementById('overlay');
+	if (el.classList) {
+	    el.classList.add('open');
+	} else {
+	    el.className += ' open';
+	}
+	loadContent('sample-output', './assets/report.html');
+}
+
+function closeSampleOutput() {
+	var el = document.getElementById('overlay');
+	if (el.classList) {
+	    el.classList.remove('open');
+	} else {
+	    el.className -= ' open';
+	}
+	var nav = document.getElementById('nav');
+	nav.innerHTML = navcontent;
+}
